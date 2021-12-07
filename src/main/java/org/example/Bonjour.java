ed6a14ef843d34d1461b038ec2dd62cf7dd0875d
Bastien Bovin <bastien.bovin@epsi.fr>
2021-12-07 10:41:23 +0100

    bonjour11

--- src/main/java/org/example/Bonjour.java
+++ src/main/java/org/example/Bonjour.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 public class Bonjour {
@@ -9,6 +11,9 @@ public class Bonjour {
             return "Bonjour, l'ami";
         } else {
             String[] allName = name.split(",");
+            List<String> lowerName;
+            List<String> upperName;
+
             if(allName.length == 1) {
                 name = allName[0];
                 if (name.equals(name.toUpperCase(Locale.ROOT))) {
@@ -17,11 +22,39 @@ public class Bonjour {
                     return "Bonjour, " + name;
                 }
             } else {
-                String finalString = "Bonjour, ";
-                for(int i = 0; i < allName.length - 2; i++){
-                    finalString += allName[i] + ", ";
+                lowerName = new ArrayList<>();
+                upperName = new ArrayList<>();
+                for (int i = 0; i < allName.length; i++) {
+                    if (allName[i].equals(allName[i].toUpperCase(Locale.ROOT))) {
+                        upperName.add(allName[i]);
+                    } else {
+                        lowerName.add(allName[i]);
+                    }
                 }
-                finalString += allName[allName.length - 2] + " et " + allName[allName.length - 1];
+                String finalString = "";
+
+                if (lowerName.size() >= 1) {
+                    finalString += "Bonjour, " + lowerName.get(0) + ".";
+                    for (int i = 0; i < lowerName.size() - 1; i++) {
+                       finalString += ", " + lowerName.get(i);
+                    }
+                    finalString += " et " + lowerName.get(lowerName.size() - 1) + ".";
+                } else {
+                    finalString += "BONJOUR";
+                }
+                if(upperName.size() >= 1){
+                    if(lowerName.size() != 0){
+                        finalString += " ET BONJOUR";
+                    }
+                    if(upperName.size() == 1){
+                        finalString += ", " + upperName.get(0) + " !";
+                    } else {
+                        for (int i = 0; i < upperName.size() - 1; i++) {
+                            finalString += ", " + upperName.get(i);
+                        }
+                        finalString += " ET " + upperName.get(upperName.size() - 1) + " !";
+                    }
+                }
                 return finalString;
             }
         }
